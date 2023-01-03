970304ad33de37395c1f89e13790bf642032a941
Michael B <bosse730@gmail.com>
2023-01-03 20:47:08 +1100

    adding search to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,5 @@
-// var name = Sydney
-// $.ajax({
-//     method: 'GET',
-//     url: 'https://api.api-ninjas.com/v1/city?name=' + name,
-//     headers: { 'X-Api-Key': 'G3W1Wgapc1pEWDW/367zqQ==imXostAsxI7vOQe9'},
-//     contentType: 'application/json',
-//     success: function(result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
+
+var input = document.querySelector("search-bar");
+
+
+localStorage.setItem("Searchedcity", input.value);
