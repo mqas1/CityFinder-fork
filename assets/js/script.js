00584c45183db27cc16295a1199334a12abd54da
Michael B <bosse730@gmail.com>
2023-01-03 19:25:14 +1100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// var name = Sydney
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/city?name=' + name,
+//     headers: { 'X-Api-Key': 'G3W1Wgapc1pEWDW/367zqQ==imXostAsxI7vOQe9'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
